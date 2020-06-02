186912143fd606f558b4de3fb2b54f8fbc58b416
Billy Mitchell <billymitchell@MacBook-Pro.verizon.net>
2020-06-02 19:34:39 -0400

    update form with netlify complience and add captia

--- src/conponents/contact.js
+++ src/conponents/contact.js
@@ -8,13 +8,21 @@ const Contact = () => {
         className="contact"
         name="contact"
         method="POST"
+        data-netlify-recaptcha="true"
+        enctype="application/x-www-form-urlencoded"
         data-netlify="true"
       >
         <div className="grid-container col-2 col-gap-5 small-col-1">
-          <input type="text" placeholder="name" />
-          <input type="email" placeholder="email" />
+          <input name="name" type="text" placeholder="name" />
+          <input name="email" type="email" placeholder="email" />
         </div>
-        <textarea type="text" placeholder="message" />
+        <textarea name="message" type="text" placeholder="message" />
+        <p class="hidden">
+          <label>
+            Don’t fill this out if you're human: <input name="bot-field" />
+          </label>
+        </p>
+        <div data-netlify-recaptcha="true"></div>
         <button type="submit" className="primary">
           Submit
         </button>
