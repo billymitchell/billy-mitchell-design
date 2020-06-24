1c41c6b87f400879396260dedcd775e9b0726c3e
Billy Mitchell <billymitchell@MacBook-Pro.verizon.net>
2020-06-24 13:32:27 -0400

    removed url meta data

--- src/components/layout/header/mettadata.js
+++ src/components/layout/header/mettadata.js
@@ -2,8 +2,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import favicon from "../../../../static/favicon.ico"
 
-const MetaData = ({ title, description, socialimg }, { location }) => {
-  var url = { location }.href
+const MetaData = ({ title, description, socialimg }) => {
   return (
     <Helmet>
       {/* Primary Meta Tags */}
@@ -14,14 +13,14 @@ const MetaData = ({ title, description, socialimg }, { location }) => {
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={url} />
+      {/* <meta property="og:url" content={url} /> */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={socialimg} />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={url} />
+      {/* <meta property="twitter:url" content={url} /> */}
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={socialimg} />
