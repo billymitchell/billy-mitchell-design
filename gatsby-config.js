bb47c9836e9836d445a11c2d679746a7814bbe1a
BillyMitchellDesign <hello@billymitchell.design>
2020-10-03 17:42:26 -0400

    gooogle analytics using gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,12 +26,14 @@ module.exports = {
         ],
       },
     },
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-      trackingId: "UA-179560174-1",
-      head: true,
-      anonymize: true,
-      respectDNT: true,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-179560174-1",
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+      },
     },
   ],
 }
